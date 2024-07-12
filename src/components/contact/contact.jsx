207f639cb61b532fe5eed4415a9fcd4c424edc79
MrKyrylo <138040487+MrKyrylo@users.noreply.github.com>
2024-07-12 23:46:13 +0300

    1

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,14 +1,19 @@
-import css from "./contact.module.css";
+import { useDispatch } from 'react-redux';
+import css from './contact.module.css';
+import { deleteContact } from '../actions/contactActions';
 
-export default function Contact({ id, name, number, delContact }) {
+export default function Contact({ id, name, number }) {
+  const dispatch = useDispatch();
+
   function onClick() {
-    delContact(id);
+    dispatch(deleteContact(id));
   }
+
   return (
     <li className={css.contact}>
       <div className="forInfo">
         <p>{name}</p>
-        <p>{number} </p>
+        <p>{number}</p>
       </div>
       <button onClick={onClick}>Delete</button>
     </li>
